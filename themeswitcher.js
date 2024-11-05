b158974de893378da90fdd4f16b323d0df8db312
JoakimJobloop <Joakim@jobloop.no>
2024-11-05 09:44:13 +0100

    first commit

---
+++ themeswitcher.js
@@ -0,0 +1,13 @@
+
+
+function themeSwitcher() {
+    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
+}
+
+/**
+ * Toggles between the "light" and "dark" classes on the document body,
+ * allowing the user to switch between light and dark themes.
+ *
+ * @return {undefined} This function does not return a value.
+ */
